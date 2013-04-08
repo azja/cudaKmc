65ffc32acd6ca3bb7e5086f4efe11b153bb11bd4
azja <andrzej.biborski@gmail.com>
2013-04-08 11:37:00 +0200

    environment

---
+++ environment/env.cuh
@@ -0,0 +1,21 @@
+/*
+ * env.h
+ *
+ *  Created on: 31-03-2013
+ *      Author: biborski
+ */
+
+#ifndef ENV_H_
+#define ENV_H_
+
+namespace cudakmc {
+
+enum verbosity {
+	verbose,
+	silent
+};
+
+}
+
+
+#endif /* ENV_H_ */
