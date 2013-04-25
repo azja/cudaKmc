77227424e40455e57d5f50baef79fe6646aba680
eaglenature <eaglenature@gmail.com>
2013-04-25 20:42:23 +0200

    Update at neighbour searching kernel

--- src/kernels.cu
+++ src/kernels.cu
@@ -1,82 +1,84 @@
 #include "../headers/kernels.cuh"
 
-__global__ void findNeigboursXyz(const float4 * const sites,
-		int4 * neigbours, float3 base1, float3 base2, float3 base3,
-		int3 dimensions, float radius, int offset, int beginFrom, int size) {
+__global__ void findNeigboursXyz(
+         const float4* const sites,
+         int4*  const neighbours,
+         float3 base1,
+         float3 base2,
+         float3 base3,
+         int3   dimensions,
+         float  radius,
+         int    offset,
+         int    beginFrom,
+         int    size)
+{
+    const int id = blockDim.x * blockIdx.x + threadIdx.x + beginFrom;
 
-	int id = blockDim.x * blockIdx.x + threadIdx.x + beginFrom;
+    // TODO pass square of radius as input parameter
+    const float squareRadius = radius * radius;
 
-	extern __shared__ float X[];
+    // TODO store as float3 structures
+    extern __shared__ float4 shared_sites[];
 
-	if (id < size) {
+    // load site data assigned for this thread
+    float4 currentSite = sites[id];
 
-		float x = sites[id].x;
-		float y = sites[id].y;
-		float z = sites[id].z;
+    int neighbourCounter = 0;
 
-		float lx;
-		float ly;
-		float lz;
+    for (int i = 0, tile = 0; i < size; i += blockDim.x, ++tile)
+    {
+        if(tile * blockDim.x + threadIdx.x < size)
+        {
+            float4 site = sites[tile * blockDim.x + threadIdx.x];
+            shared_sites[threadIdx.x] = site;
+        }
+        else
+        {
+            shared_sites[threadIdx.x] = make_float4(0.0f, 0.0f, 0.0f, 0.0f);
+        }
 
-		int nCntr = 0;
+        __syncthreads();
 
-		for (int j = 0; j < gridDim.x; ++j) {
-			__syncthreads();
-			if ((threadIdx.x + blockDim.x * j) < size) {
+        for (int isite = 0; isite < blockDim.x; ++isite)
+        {
+            if ((tile * blockDim.x + isite != id) && (tile * blockDim.x + isite < size))
+            {
+                float4 site = shared_sites[isite];
 
-				X[threadIdx.x] = sites[threadIdx.x + blockDim.x * j].x;
-				X[threadIdx.x + blockDim.x] =
-						sites[threadIdx.x + blockDim.x * j].y;
-				X[threadIdx.x + 2 * blockDim.x] = sites[threadIdx.x
-				                                        + blockDim.x * j].z;
-				__syncthreads();
+                float xp = site.x - currentSite.x;
+                float yp = site.y - currentSite.y;
+                float zp = site.z - currentSite.z;
 
-				for (int i = 0; i < blockDim.x; ++i) {
-					if ((j * blockDim.x + i) != id) {
+                #pragma unroll
+                for (int k = -1; k <= 1; ++k)
+                {
+                    #pragma unroll
+                    for (int l = -1; l <= 1; ++l)
+                    {
+                        #pragma unroll
+                        for (int m = -1; m <= 1; ++m)
+                        {
+                            float lx = k * dimensions.x * base1.x + l * dimensions.y * base2.x + m * dimensions.z * base3.x;
+                            float ly = k * dimensions.x * base1.y + l * dimensions.y * base2.y + m * dimensions.z * base3.y;
+                            float lz = k * dimensions.x * base1.z + l * dimensions.y * base2.z + m * dimensions.z * base3.z;
 
-						float xp = X[i] - x;
-						float yp = X[i + blockDim.x] - y;
-						float zp = X[i + 2 * blockDim.x] - z;
-#pragma unroll
-						for (int k = -1; k <= 1; ++k) {
-#pragma unroll
-							for (int l = -1; l <= 1; ++l) {
-#pragma unroll
-								for (int m = -1; m <= 1; ++m) {
+                            float squareDistance = (xp + lx) * (xp + lx) + (yp + ly) * (yp + ly) + (zp + lz) * (zp + lz);
 
-									lx = k * dimensions.x * base1.x
-											+ l * dimensions.y * base2.x
-											+ m * dimensions.z * base3.x;
-									ly = k * dimensions.x * base1.y
-											+ l * dimensions.y * base2.y
-											+ m * dimensions.z * base3.y;
-									lz = k * dimensions.x * base1.z
-											+ l * dimensions.z * base2.z
-											+ m * dimensions.z * base3.z;
+                            // TODO use fabs
+                            if ((neighbourCounter < offset) && (squareDistance < squareRadius))
+                            {
+                                neighbours[id * offset + neighbourCounter] = make_int4(-k, -l, -m, tile * blockDim.x + isite);
+                                ++neighbourCounter;
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-									float distance = sqrtf(
-											(xp + lx) * (xp + lx)
-											+ (yp + ly) * (yp + ly)
-											+ (zp + lz) * (zp + lz));
+        __syncthreads();
 
-									if (distance < radius && nCntr < offset) {
-
-										neigbours[id * offset + nCntr].x = -k;
-										neigbours[id * offset + nCntr].y = -l;
-										neigbours[id * offset + nCntr].z = -m;
-										neigbours[id * offset + nCntr].w = j
-												* blockDim.x + i;
-										nCntr++;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-
-	}
+    }
 }
 
 
@@ -96,31 +98,31 @@ __global__ void findNeigboursXyz(const float4 * const sites,
  *
  */
 
- template<typename SimulationInput>
+template<typename SimulationInput>
 __global__ void calculateTransitionParameter(SimulationInput input) {
 
-	typename SimulationInput::EnergyExchange exchangeCalcer;
-	typename SimulationInput::SaddleEnergyFunctor saddleCalcer;
+    typename SimulationInput::EnergyExchange exchangeCalcer;
+    typename SimulationInput::SaddleEnergyFunctor saddleCalcer;
 
-	int thId = blockIdx.x * blockDim.x + threadIdx.x;
-	if (thId < input.N) {
-		int id1 = input.vacancies[thId];
+    int thId = blockIdx.x * blockDim.x + threadIdx.x;
+    if (thId < input.N) {
+        int id1 = input.vacancies[thId];
 
-		for (int i = 0; i < input.z_t; ++i) {
-			int id2 = input.jumpingNeigbours[thId * input.z_t + i].w;
+        for (int i = 0; i < input.z_t; ++i) {
+            int id2 = input.jumpingNeigbours[thId * input.z_t + i].w;
 
-			input.transitions[thId * input.z_t + i] =
-			/*0.5 x dE */
+            input.transitions[thId * input.z_t + i] =
+            /*0.5 x dE */
 
-				0.5	* exchangeCalcer(id1, id1, input.sites, input.neigbours,
-							input.N, input.exchangeEnergyCalculation,
-							input.atoms_n)
-					+
-					/* Es */
-					saddleCalcer(id1, id2, input.sites, input.neigbours,
-							input.saddleEnergyCalculation);
+                0.5	* exchangeCalcer(id1, id1, input.sites, input.neigbours,
+                            input.N, input.exchangeEnergyCalculation,
+                            input.atoms_n)
+                    +
+                    /* Es */
+                    saddleCalcer(id1, id2, input.sites, input.neigbours,
+                            input.saddleEnergyCalculation);
 
-		}
-	}
+        }
+    }
 
 }
